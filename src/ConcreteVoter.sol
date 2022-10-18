d59663b1dc4060930354f0b3342ae780c90e78a7
Chainvisions <chainvisions@outlook.com>
2022-10-17 21:00:16 -0600

    Solidity 0.8.13 -> 0.8.16

--- src/ConcreteVoter.sol
+++ src/ConcreteVoter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity 0.8.16;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ITokenLocker} from "./interfaces/concrete/ITokenLocker.sol";
